52624bcdec358ca1982f2995f3fdffd1475fae2a
marcodldev <marcodelisi2@gmail.com>
2022-11-07 16:02:51 +0100

    + snack 2

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ e dati due numeri min e max (min più piccolo di max).
 Creiamo un nuovo array contenente i valori che hanno la posizione compresa tra i due numeri.
 Eseguiamo questo esercizio prima con forEach e poi con filter.
 esempio: dati i valori min: 2 e come max: 4 i nomi nell'array nuovo saranno i nomi compresi tra queste posizioni 2 e 4
+
 Snack 2
 Dato un elenco degli studenti di una facoltà, con il totale dei loro voti
 let students = [
@@ -19,3 +20,42 @@ let students = [
 1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.
 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120 */
+
+let students = [
+    { name: 'Marco', id: 213, grades: 78 },
+    { name: 'Paola', id: 110, grades: 96 },
+    { name: 'Andrea', id: 250, grades: 48 },
+    { name: 'Gaia', id: 145, grades: 74 },
+    { name: 'Luigi', id: 196, grades: 68 },
+    { name: 'Piero', id: 102, grades: 50 },
+    { name: 'Francesca', id: 120, grades: 84 },
+  ]
+;
+
+const capital = students.map( (element) => {
+
+    cap = (element.name).toUpperCase();
+    return cap;
+});
+
+console.log(capital);
+
+const grades = students.filter((students) => {
+    if (students.grades > 70 ) {
+        return true;
+    }
+    return false;
+
+});
+
+console.log(grades); 
+
+const grades_and_id = students.filter((students) => {
+    if (students.grades > 70 && students.id > 120) {
+        return true;
+    }
+    return false;
+
+});
+
+console.log(grades_and_id); 
